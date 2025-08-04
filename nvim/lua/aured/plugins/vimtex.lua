d9ec7abd110815054abe5c224ff9e7ff1329de20
pandaaured <118323804+pandaaured@users.noreply.github.com>
2025-08-04 13:10:22 -0400

    Update vimtex.lua
    Added comment for vimtex_view_method

--- nvim/lua/aured/plugins/vimtex.lua
+++ nvim/lua/aured/plugins/vimtex.lua
@@ -2,6 +2,6 @@ return {
   "lervag/vimtex",
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = "general"
+    vim.g.vimtex_view_method = "general" -- Setting for PDF viewer (to be opened upon :VimtexCompile)
   end
 }
