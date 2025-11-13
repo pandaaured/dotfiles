10b3248d0ab5d1bb634048667b92322df57c8676
pandaaured <118323804+pandaaured@users.noreply.github.com>
2025-11-13 13:22:59 -0500

    Update keymaps
    Move keymaps from files to keymaps.lua

--- .config/nvim/lua/aured/config/keymaps.lua
+++ .config/nvim/lua/aured/config/keymaps.lua
@@ -1,37 +1,53 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "  "
 
-local keymap = vim.keymap -- concisenes
+local keymap = vim.keymap -- conciseness
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" }) 
 
 -- increment/decrement numbers
-
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Splitwindow vertically" }) -- split window horizontal
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Splitwindow horizontally" }) -- split window vertical
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Splits equal size" }) -- split window equal size
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- split window close
 
 -- tab management
-
-
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabnp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- 
 
+-- tree management
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" }) -- toggle file explorer
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle File Explorer on current" }) -- toggle file explorer on current
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" }) -- collapse file explorer
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" }) -- refresh file explorer
 
--- Telescope management 
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" }) 
+-- telescope management 
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" }) 
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 
+-- trouble 
+keymap.set("n", "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Open trouble workspace diagnostics" })
+keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Open trouble document diagnostics" })
+keymap.set("n", "<leader>xd", "<cmd>Trouble quickfix toggle<CR>", { desc = "Open trouble quickfix list" })
+keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<CR>", { desc = "Open trouble location list" })
+keymap.set("n", "<leader>xt", "<cmd>Trouble todo toggle<CR>", { desc = "Open todos in trouble" })
+
+-- outlines
+keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+-- lazygit
+keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>",  { desc = "Open lazygit" })
+
+-- live server
+keymap.set("n", "<leader>ls", "<cmd>LiveServerStart<CR>", { desc = "Live Server Start" })
+keymap.set("n", "<leader>lr", "<cmd>LiveServerStop<CR>", { desc = "Live Server Stop" })
