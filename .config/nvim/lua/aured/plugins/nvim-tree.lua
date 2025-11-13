1f9fa52193f3ea9d0ed605b66530b38561cf6c2c
pandaaured <118323804+pandaaured@users.noreply.github.com>
2025-11-13 13:29:08 -0500

    Removed keymaps from nvim-tree.lua

--- .config/nvim/lua/aured/plugins/nvim-tree.lua
+++ .config/nvim/lua/aured/plugins/nvim-tree.lua
@@ -28,12 +28,5 @@ return {
         ignore = false,
       },
     })
-
-    -- set keymaps
-    local keymap = vim.keymap -- conciseness
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle File Explorer on current" }) -- toggle file explorer on current
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" }) -- refresh file explorer
   end
 }
