5a7e56afc5b83e69c41f5619a9ef226e5753ba47
pandaaured <118323804+pandaaured@users.noreply.github.com>
2025-11-13 13:26:20 -0500

    Remove keybinds from outline.lua

--- .config/nvim/lua/aured/plugins/outline.lua
+++ .config/nvim/lua/aured/plugins/outline.lua
@@ -2,9 +2,6 @@ return {
   "hedyhli/outline.nvim",
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
-  keys = {
-    {"<leader>o", "<cmd>Outline<CR>", desc = "Toggle Outline" },
-  },
   opts = {
     outline_window = {
       position = 'right',
