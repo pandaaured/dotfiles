e4f09d306df785ef27b8e2551e0f5945e1352b49
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-07-14 12:38:21 -0400

    Add the typst plugin

--- .zshrc
+++
@@ -1,4 +0,0 @@
-alias 213="cd ~/repos/academics/15-213/m26"
-alias river="ssh liamwalk@rivershark.ics.cs.cmu.edu"
-
-eval "$(starship init zsh)"
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ for f in .vimrc .wezterm.lua .zprofile .zshrc .zshenv; do
   ln -sf "$PWD/$f" ~/$f
 done
 
-for f in nvim starship.toml uv; do
+for f in nvim starship.toml uv clangd; do
   ln -sf "$PWD/.config/$f" ~/.config/$f
 done
 
