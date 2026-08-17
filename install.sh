6df2ce3d2c4528f7793a1fa36847c19b0e909903
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-08-17 15:53:33 -0400

    Updated install script and added alacritty again

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ for f in .vimrc .wezterm.lua .zprofile .zshrc .zshenv; do
   ln -sf "$PWD/$f" ~/$f
 done
 
-for f in nvim starship.toml uv clangd; do
+for f in nvim starship.toml uv clangd alacritty/alacritty.toml; do
   ln -sf "$PWD/.config/$f" ~/.config/$f
 done
 
