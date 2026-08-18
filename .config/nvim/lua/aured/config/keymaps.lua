9e8a07220006518363da7446cd887aa9af417812
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-08-18 10:25:55 -0400

    Keymaps

--- .config/nvim/lua/aured/config/keymaps.lua
+++ .config/nvim/lua/aured/config/keymaps.lua
@@ -1,68 +1,51 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "  "
 
-local keymap = vim.keymap -- conciseness
+local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" }) 
+-- Core editor bindings. 
+-- <leader>e* are bindings corresponding to nvim-tree.lua
+-- <leader>d* are bindings corresponding to trouble.lua
+-- <leader>ss is a binding corresponding to outline
+-- <leader>f* are bindings corresponding to telescope.lua
+-- <leader>gg are bindings corresponding to lazygit.lua
+-- <leader>g* are bindings corresponding to gitsigns.lua
+    -- these are buffer local on attach.
+-- <leader>c/l/r/s*, g* are bindings corresponding to lsp.lua.
+    -- these are buffer local on attach.
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+---- MISC --------------------------------------------------------------------
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Splitwindow vertically" }) -- split window horizontal
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Splitwindow horizontally" }) -- split window vertical
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Splits equal size" }) -- split window equal size
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- split window close
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- 
+---- [WINDOW] ----------------------------------------------------------------
 
--- tree management
-keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" }) -- toggle file explorer
-keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle File Explorer on current" }) -- toggle file explorer on current
-keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" }) -- collapse file explorer
-keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" }) -- refresh file explorer
+keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>wh", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "Equalise splits" })
+keymap.set("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close split" })
 
--- telescope management 
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+keymap.set("n", "<A-h>", "<C-w>h", { desc = "Window left" })
+keymap.set("n", "<A-j>", "<C-w>j", { desc = "Window down" })
+keymap.set("n", "<A-k>", "<C-w>k", { desc = "Window up" })
+keymap.set("n", "<A-l>", "<C-w>l", { desc = "Window right" })
 
--- trouble 
-keymap.set("n", "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Open trouble workspace diagnostics" })
-keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Open trouble document diagnostics" })
-keymap.set("n", "<leader>xd", "<cmd>Trouble quickfix toggle<CR>", { desc = "Open trouble quickfix list" })
-keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<CR>", { desc = "Open trouble location list" })
-keymap.set("n", "<leader>xt", "<cmd>Trouble todo toggle<CR>", { desc = "Open todos in trouble" })
+---- [TERMINAL] --------------------------------------------------------------
 
--- outlines
-keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h", { desc = "Window left" })
+keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j", { desc = "Window down" })
+keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k", { desc = "Window up" })
+keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l", { desc = "Window right" })
 
--- lazygit
-keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>",  { desc = "Open lazygit" })
+---- [TABS] ------------------------------------------------------------------
 
--- live server
-keymap.set("n", "<leader>ls", "<cmd>LiveServerStart<CR>", { desc = "Live Server Start" })
-keymap.set("n", "<leader>lr", "<cmd>LiveServerStop<CR>", { desc = "Live Server Stop" })
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Buffer in new tab" })
 
--- Exit terminal mode.
-keymap.set('t', '<Esc>', '<C-\\><C-n>')
-
--- Terminal mode.
-keymap.set('t', '<A-h>', '<C-\\><C-n><C-w>h')
-keymap.set('t', '<A-j>', '<C-\\><C-n><C-w>j')
-keymap.set('t', '<A-k>', '<C-\\><C-n><C-w>k')
-keymap.set('t', '<A-l>', '<C-\\><C-n><C-w>l')
-
--- Normal mode
-keymap.set('n', '<A-h>', '<C-w>h')
-keymap.set('n', '<A-j>', '<C-w>j')
-keymap.set('n', '<A-k>', '<C-w>k')
-keymap.set('n', '<A-l>', '<C-w>l')
