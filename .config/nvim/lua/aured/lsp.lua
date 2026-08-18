0bee56ecd409c0503a917e7dd80e3bf908ea02a0
pandaaured <118323804+pandaaured@users.noreply.github.com>
2026-08-18 09:03:43 -0400

    Update LSP keyboard bindings.
    Add [edit], [find], [goto], [info], [lsp], [nav] comments to better
    sort by functionality.

--- .config/nvim/lua/aured/lsp.lua
+++ .config/nvim/lua/aured/lsp.lua
@@ -17,56 +17,85 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
 
+    -- [lsp] inlay hints are enabled by default.
     vim.lsp.inlay_hint.enable(true)
     local opts = { buffer = ev.buf, silent = true }
 
-    opts.desc = "Show LSP references."
-    vim.keymap.set("n", "gR", "<cmd>Telescope lsp_reference<CR>", opts)
+    ---- [edit] ---------------------------------------------------------------
 
-    opts.desc = "Go to declaration."
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-    opts.desc = "Show LSP definition."
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-    opts.desc = "Show LSP implementations."
-    vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementation<CR>", opts)
-
-    opts.desc = "Show LSP type definitions"
-    vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-
-    opts.desc = "See available code actions"
+    opts.desc = "[edit] code action"
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-    opts.desc = "Smart rename"
+    opts.desc = "[edit] rename"
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-    opts.desc = "Show buffer diagnostics"
-    vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) 
+    ---- [find] ---------------------------------------------------------------
 
-    opts.desc = "Show line diagnostics"
+    opts.desc = "[find] buffer diagnostics"
+    vim.keymap.set("n", "<leader>D",
+      "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+    opts.desc = "[find] document symbols"
+    vim.keymap.set("n", "<leader>ds",
+      "<cmd>Telescope lsp_document_symbols<CR>", opts)
+
+    opts.desc = "[find] incoming calls"
+    vim.keymap.set("n", "<leader>ci", vim.lsp.buf.incoming_calls, opts)
+
+    opts.desc = "[find] workspace symbols"
+    vim.keymap.set("n", "<leader>ws",
+      "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
+
+    opts.desc = "[find] references"
+    vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+    ---- [goto] ---------------------------------------------------------------
+
+    opts.desc = "[goto] declaration"
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+    opts.desc = "[goto] definition"
+    vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+    opts.desc = "[goto] implementations"
+    vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementation<CR>", opts)
+
+    opts.desc = "[goto] supertypes"
+    vim.keymap.set("n", "<leader>th",
+      function() vim.lsp.buf.typehierarchy("supertypes") end, opts)
+
+    opts.desc = "[goto] type definitions"
+    vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+    ---- [info] ---------------------------------------------------------------
+
+    opts.desc = "[info] line diagnostics"
     vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-    opts.desc = "Go to previous diagnostic"
-    vim.keymap.set("n", "[d",
-      function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+    opts.desc = "[info] signature help"
+    vim.keymap.set({"n", "i"}, "<C-k>", vim.lsp.buf.signature_help, opts)
 
-    opts.desc = "Go to next diagnostic"
+    ---- [lsp] ----------------------------------------------------------------
+
+    opts.desc = "[lsp] restart"
+    vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+
+    opts.desc = "[lsp] toggle inlay hints"
+    vim.keymap.set("n", "gh", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      end, opts)
+
+    ---- [nav] ----------------------------------------------------------------
+
+    opts.desc = "[nav] prev diagnostic"
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.jump({ count = -1, float = true })
+    end, opts)
+
+    opts.desc = "[nav] next diagnostic"
     vim.keymap.set("n", "]d", function()
       vim.diagnostic.jump({ count = 1, float = true })
     end, opts)
-
-    opts.desc = "Show documentation for what is under cursor"
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-    opts.desc = "Restart LSP"
-    vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-
-    opts.desc = "Toggle LSP Inlay Hints"
-    vim.keymap.set("n", "gh", function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) 
-      end, 
-    opts)
 
   end,
 })
